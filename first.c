0ad0bbbd7b0cb6b776bbcfd400ba65c6bb3d303c
boo-boopathy <boopathyksrct@gmail.com>
2017-06-14 15:20:21 +0530

    Create first.c

---
+++ first.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    double number;
+    printf("Enter a number: ");
+    scanf("%lf", &number);
+    if (number <= 0.0)
+    {
+        if (number == 0.0)
+            printf("You entered 0.");
+        else
+            printf("You entered a -ve number.");
+    }
+    else
+        printf("You entered a +ve number.");
+    return 0;
+}
